d594d9d567ff43ffe095014c2ee077dc74907016
David J. Davydov <davydovjd611@gmail.com>
2020-12-19 07:07:17 -0500

    Update the file to include new apps

--- expressApps/appReducer.js
+++ expressApps/appReducer.js
@@ -3,8 +3,12 @@ const express = require('express');
 const subdomain = require('express-subdomain');
 // Apps
 const apiApp = require('./api/app');
+const ircApp = require('./irc/app');
+const websiteApp = require('./website/app');
 
 const app = express();
 app.use(subdomain('api', apiApp));
+app.use(subdomain('app', ircApp));
+app.use(websiteApp);
 
 module.exports = app;
